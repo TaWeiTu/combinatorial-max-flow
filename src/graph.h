db405cce1e98c8f020a9bb36356ee97a9370d65c
Joakim Blikstad <jockemocke@gmail.com>
2024-12-04 14:55:34 +0100

    improve graph api

--- src/graph.h
+++ src/graph.h
@@ -1,24 +1,44 @@
 #pragma once
 
+#include <ranges>
 #include <vector>
 
-using Vertex = int;
-using Edge = int;
-using CapacityT = int64_t;
+#include "types.h"
 
 struct Graph {
+  int n = 0, m = 0;
+
   std::vector<std::vector<Edge>> out_edges;
   std::vector<std::vector<Edge>> in_edges;
-  std::vector<CapacityT> cap;
+  std::vector<CapacityT> capacity;
   std::vector<Vertex> head, tail;
 
-  Graph Residual(const std::vector<CapacityT> &flow) const;
-
-  void AddEdge(Vertex h, Vertex t) {
-    out_edges[h].push_back(head.size());
-    in_edges[t].push_back(head.size());
-    head.push_back(h);
+  Edge AddEdge(Vertex t, Vertex h, CapacityT c = 0) {
+    out_edges[t].push_back(m);
+    in_edges[h].push_back(m);
     tail.push_back(t);
-    cap.push_back(0);
+    head.push_back(h);
+    capacity.push_back(c);
+    return m++;
   }
+  Vertex AddVertex() {
+    out_edges.emplace_back();
+    in_edges.emplace_back();
+    return n++;
+  }
+  static Graph FromEdgeList(
+      int n, const std::vector<std::tuple<Vertex, Vertex, CapacityT>>& edges) {
+    Graph g;
+    while (g.n < n) g.AddVertex();
+    for (auto [x, y, c] : edges) g.AddEdge(x, y, c);
+    return g;
+  }
+
+  auto Vertices() const { return std::ranges::views::iota(0, n); }
+  auto Edges() const { return std::ranges::views::iota(0, m); }
+  std::tuple<Vertex, Vertex, CapacityT> EdgeInfo(Edge e) const {
+    return {tail[e], head[e], capacity[e]};
+  }
+
+  Graph Residual(const std::vector<CapacityT>& flow) const;
 };
---
+++ src/types.h
@@ -0,0 +1,8 @@
+#pragma once
+
+#include <cstdint>
+
+using Vertex = int;
+using Edge = int;
+using CapacityT = int64_t;
+using WeightT = int;
