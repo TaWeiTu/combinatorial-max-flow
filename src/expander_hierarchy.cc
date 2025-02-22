13c0176f6bb5a820040e6fdbc157344adc2db620
Ta-Wei Tu <tu.da.wei@gmail.com>
2025-02-21 18:18:11 -0800

    Add expander hierarchy

---
+++ src/expander_hierarchy.cc
@@ -0,0 +1,74 @@
+#include "expander_hierarchy.h"
+
+#include <algorithm>
+#include <cassert>
+#include <cmath>
+#include <numeric>
+
+void FlowUnfolding::AddLevel(ShortcutGraph sg,
+                             std::unique_ptr<Witness> witness) {
+  sg_.push_back(sg);
+  witness_.push_back(std::move(witness));
+}
+
+std::vector<CapacityT> FlowUnfolding::Unfold(
+    std::vector<CapacityT> flow_on_shortcut) {
+  for (int l = sg_.size() - 1; l > 0; --l) {
+    assert(std::ssize(flow_on_shortcut) == sg_[l].shortcut.m &&
+           "flow_on_shortcut is a flow on the level-l shortcut graph");
+    assert(std::ranges::all_of(
+               sg_[l].shortcut.Edges(),
+               [&, k = sg_.size() - l](Edge e) {
+                 return flow_on_shortcut[e] <=
+                        (sg_[l].IsStarEdge(e)
+                             ? sg_[l].shortcut.capacity[e] * k
+                             : sg_[l].shortcut.capacity[e] / scale_ *
+                                   (scale_ + inv_phi_ * k * (k - 1) / 2));
+               }) &&
+           "the capacity blows on by a factor of (1+1/L) each time");
+    std::vector<CapacityT> demand(g_.n);
+    assert(sg_[l].L == l);
+    for (Vertex v : g_.Vertices()) {
+      demand[v] +=
+          flow_on_shortcut[sg_[l].StarEdge(l, v, ShortcutGraph::kToStar)];
+      demand[v] -=
+          flow_on_shortcut[sg_[l].StarEdge(l, v, ShortcutGraph::kFromStar)];
+    }
+    auto reroute = witness_[l]->Route(demand);
+    assert(std::ssize(reroute) == sg_[l - 1].shortcut.m &&
+           "rerouted on lower level shortcut graph");
+    assert(std::ranges::all_of(
+        sg_[l - 1].shortcut.Edges(), [&, k = sg_.size() - 1](Edge e) {
+          return reroute[e] <= (sg_[l - 1].IsStarEdge(e)
+                                    ? sg_[l - 1].shortcut.capacity[e]
+                                    : sg_[l - 1].shortcut.capacity[e] / scale_ *
+                                          (scale_ + inv_phi_ * k));
+        }));
+    flow_on_shortcut = reroute;
+  }
+  return flow_on_shortcut;
+}
+
+std::pair<std::vector<int>, FlowUnfolding> BuildExpanderHierarchy(Graph g) {
+  std::vector<int> level(g.m, 0);
+  CapacityT total_capacity =
+      std::accumulate(g.capacity.begin(), g.capacity.end(), CapacityT(0));
+  // L = ceil(log_{4/3}(c(E))) + 1
+  const int L = CapacityT(std::ceil(std::log2(total_capacity) /
+                                    std::log2(double(4) / 3))) +
+                1;
+  // TODO: Check these values.
+  const CapacityT inv_phi = CapacityT(std::pow(std::log2(g.n), 10));
+  // This corresopnds to 1/psi in the paper.
+  // Note that in the paper psi is set to phi/L but here we set it to psi/L^2.
+  // This is so that we can stay integral throughout the implementation.
+  const CapacityT scale = L * L * inv_phi;
+  FlowUnfolding fu(g, scale, inv_phi);
+  while (true) {
+    auto [new_level, witness, sg] = ExpanderDecomposition(g, level, scale);
+    if (level == new_level) break;
+    fu.AddLevel(sg, std::move(witness));
+    level = new_level;
+  }
+  return std::make_pair(level, std::move(fu));
+}
--- src/expander_hierarchy.h
+++ src/expander_hierarchy.h
@@ -1,13 +1,25 @@
 #pragma once
 
+#include <vector>
+
+#include "expander_decomposition.h"
 #include "graph.h"
-#include "shortcut_graph.h"
 
-class ExpanderHierarchy {
+class FlowUnfolding {
  public:
-  ExpanderHierarchy(const Graph& g);
-  std::vector<CapacityT> UnwrapFlow(
-      const std::vector<CapacityT>& flow_on_shortcut_graph);
+  FlowUnfolding(Graph g, CapacityT scale, CapacityT inv_phi)
+      : g_(g), scale_(scale), inv_phi_(inv_phi) {}
+  void AddLevel(ShortcutGraph sg, std::unique_ptr<Witness> witness);
 
-  ShortcutGraph shortcut_graph_;
+  // Unfold the flow on the shortcut graph back to the original graph.
+  // NOTE: the flow_on_shortcut is already scaled up by a factor of 1/psi.
+  std::vector<CapacityT> Unfold(std::vector<CapacityT> flow_on_shortcut);
+
+ private:
+  std::vector<std::unique_ptr<Witness>> witness_;
+  std::vector<ShortcutGraph> sg_;
+  Graph g_;
+  CapacityT scale_, inv_phi_;
 };
+
+std::pair<std::vector<int>, FlowUnfolding> BuildExpanderHierarchy(Graph g);
